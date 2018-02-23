b51952cf14bc0e6dba28a31de1c894fbbdffc363
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-02-23 22:39:25 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop (arr) {
+  for (var i=0; i<25; ++i) {
+    arr.push(`I am ${i} strange loops.`);
+  }
+  return arr;
+}
+
+function whileLoop (n) {
+  while (n >= 0) {
+    console.log(`{n}`);
+    n--;
+  }
+  return 'done';
+}
+
+function doWhileLoop(arr) {
+  do {
+    all.
+  } while ((arr.length > 0) && maybeTrue())
+}
